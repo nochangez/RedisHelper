a4ee4872c448482f51dbbd6f9b55d59e15045f01
nochangez <62546668+nochangez@users.noreply.github.com>
2022-04-08 21:18:07 +0300

    Add files via upload

--- redis_helper.py
+++ redis_helper.py
@@ -1,46 +1,99 @@
 # coding=utf-8
 
 
-import redis
+from typing import Any
+
+from redis import Redis
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
 
 
 class RedisHelper:
-    def __init__(self):
-        # create redis connection
-        try:
-            self.redis_client = redis.Redis()  # create redis client
-        except redis.exceptions as redis_error:
-            print(f"[redis] some error has occurred: {redis_error}")
-        finally:
-            self.redis_client.close()  # close redis connection if we got some exception
-
-        self.redis_storage = RedisStorage2(
-            host="localhost", port=6379, db=0
-        )
+    def __init__(self, hostname: str = "localhost", port: int = 6379, db: int = 0):
+        # redis connections
+        self.redis_client = Redis()  # redis client connection
+        self.redis_storage = RedisStorage2(hostname, port, db)  # redis storage connection
 
     @staticmethod
-    async def decode_bytes(encoded_value):
+    async def decode_bytes(encoded_value: Any) -> Any:
         """
+        gets redis byte data and returns decoded data
+
         :param encoded_value: data to decode
-        :return: gets redis byte data and returns decoded data
+        :return: decoded data, type -> Any
         """
 
-        return encoded_value.decode("utf-8")
+        return encoded_value.decode("utf-8")  # return decoded value
 
-    async def redis_set(self, name, value):
+    async def redis_set(self, name: str, value: Any) -> bool:
         """
+        creates a note with key data on redis server
+
         :param name: key name
         :param value: key value
-        :return: bool, creates a note with key data on redis server
+        :return: bool
         """
 
-        return self.redis_client.set(name=name, value=value)
+        return self.redis_client.set(name=name, value=value)  # create key in redis
 
-    async def close_storage(self):
+    async def redis_get(self, name: str) -> bytes:
         """
-        :return: closes redis storage and clear data
+        this method gets a value from
+
+        :param name: name of a value
+        :return: bytes, value from operative memory
         """
 
-        await self.redis_storage.close()
-        await self.redis_storage.wait_closed()
+        return self.redis_client.get(name=name)  # get data via its name
+
+    async def redis_flushall(self) -> bool:
+        """
+        deletes all keys from redis server
+
+        :return: bool
+        """
+
+        return self.redis_client.flushall()  # delete all keys
+
+    async def close_redis_client(self):
+        """
+        closes redis client
+
+        :return: None
+        """
+
+        return self.redis_client.close()
+
+    async def close_redis(self):
+        """
+        close redis storage and redis client
+
+        :return: list, list of close connections methods
+        """
+
+        return [await self.close_redis_client(),
+                await self.redis_storage.close(),
+                await self.redis_storage.wait_closed()]
+
+
+# create context manager for RedisHelper
+class RedisHelperManager:
+    def __init__(self):
+        self.__redis_helper = RedisHelper()  # init object for communication with redis helper class
+
+    def __enter__(self):
+        """
+        create open point for context manager
+
+        :return: RedisHelper() open
+        """
+
+        return self.__redis_helper  # enter in context manager and call RedisHelper init
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """
+        create close point for context manager
+
+        :return: RedisHelper() close
+        """
+
+        return self.__redis_helper.close_redis()  # exit from context manager and close redis connections
